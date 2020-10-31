1660f76284a7d691a3d105340a7abc534a9e7a26
bobrobbow <67859015+bobrobbow@users.noreply.github.com>
2020-10-31 18:46:45 +0000

    remove redundant code leftover from detaching from gamecode

--- scripts/map_controls.gd
+++ scripts/map_controls.gd
@@ -18,10 +18,6 @@ func _input(event):
 		get_parent().get_node("map").set_self_modulate(Color(1.0,1.0,1.0,1.0))
 	if event.is_action_pressed(("ui_focus_next")):
 		get_parent()._generate_heat_map()
-	if event.is_action_pressed("rightclick"):
-		pass
-	elif event.is_action_released("rightclick"):
-		right_click_captured = false
 	if event is InputEventMouseMotion:
 		var mousepos = get_global_mouse_position()
 		var coordpos = get_parent().map_tilemap.world_to_map( mousepos )
@@ -38,22 +34,5 @@ func _input(event):
 			temperature_str = str(get_parent().temperature_map[coordpos])
 		right_gui.get_node("cursor_tile_text").set_text("terrain at cursor is : " + terrain_name )
 		right_gui.get_node("cursor_tile_text2").set_text("temperature :" + temperature_str)
-		
-func _process(_delta):
-	right_gui.get_node("fps_text").set_text( str(Engine.get_frames_per_second()))
-	update()
 
-func _draw():
-	var sizex = tilemap_cell_size.x
-	var sizey = tilemap_cell_size.y
-	var x = highlighted_coord.x * sizex
-	var y = highlighted_coord.y * sizey
-	# first horizontal line
-	draw_line( Vector2( x, y ), Vector2( x + sizex, y ), color )
-	# second horizontal line
-	draw_line( Vector2( x, y + sizey ), Vector2( x + sizex, y + sizey ) , color )
-	# first vertical line
-	draw_line( Vector2( x, y ), Vector2( x, y + sizey ), color )
-	# second horizontal line
-	draw_line( Vector2( x + sizex, y ), Vector2( x + sizex, y + sizey ), color )
 
